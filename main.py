133b4027f43d55ab48b497ef0bd9e2f1dba83d6d
Dmitry Veber <dmitry11374@yahoo.com>
2022-09-19 18:30:40 -0500

    added sessions

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, session, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, HiddenField
+from wtforms.validators import InputRequired, EqualTo, Length
 import random, math
 
 
@@ -12,14 +15,39 @@ def isColorLight(rgb=[0,128,255]):
 
 
 app = Flask(__name__)
+app.secret_key = "SecretKey"
 num = random.randint(0, 999)
 bg_color_vals = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
 bg_color = "#" + "".join(hex(x)[2:] for x in bg_color_vals)
 fg_color = "#000" if isColorLight(rgb=bg_color_vals) else "#fff"
 
 
-@app.route('/')
+class SessionForm(FlaskForm):
+    open_session = SubmitField("Open Session")
+    close_session = SubmitField("Close Session")
+
+
+@app.route('/', methods=["GET", "POST"])
 def hello():
+    form = SessionForm()
+
+    if form.open_session.data:
+        session["token"] = "".join([hex(random.randint(0,15))[2:] for x in range(32)])
+
+    if form.close_session.data:
+        session.clear()
+
+    if "token" in session:
+        session_html = """
+        <form method="POST" action="{}">{} {}<div>Token: {}</div></form>
+        """
+        session_html = session_html.format(url_for("hello"), form.hidden_tag(), form.close_session(), session["token"])
+    else:
+        session_html = """
+        <form method="POST" action="{}">{}{}</form>
+        """
+        session_html = session_html.format(url_for("hello"), form.hidden_tag(),  form.open_session())
+
     result = """
         <html>
         <body>
@@ -32,10 +60,11 @@ def hello():
             ">
                 {:0>3d}
             </div>
+            {}
         </body>
         </html>
     """
-    result = result.format(bg_color, fg_color, num)
+    result = result.format(bg_color, fg_color, num, session_html)
     return result
 
 
