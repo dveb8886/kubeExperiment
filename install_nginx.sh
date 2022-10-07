4ba275eeb303856f5dafa37fc22ec6d8b888da00
Dmitry Veber <dmitry11374@yahoo.com>
2022-10-06 20:40:26 -0500

    added nginx ingress installer

---
+++ install_nginx.sh
@@ -0,0 +1,23 @@
+
+
+STARTING_DIR=$(pwd)
+PRJ_DIR=${0%/*}
+HELM_CMD=${1:-install}
+HELM_BRANCH=${2:-release-1.12}
+
+cd ${PRJ_DIR} || exit
+if [ ! -d "kube-ingress" ]; then
+  git clone https://github.com/nginxinc/kubernetes-ingress.git --single-branch --branch ${HELM_BRANCH} kube-ingress
+else
+  cd kube-ingress || exit
+  git pull
+  cd ../
+fi
+cd ${STARTING_DIR} || exit
+
+if [[ "${HELM_CMD}" == "uninstall" ]]; then
+  helm ${HELM_CMD} nic
+else
+  helm ${HELM_CMD} --values ${PRJ_DIR}/helm/ingress-nginx/values.yaml nic ${PRJ_DIR}/kube-ingress/deployments/helm-chart
+fi
+
